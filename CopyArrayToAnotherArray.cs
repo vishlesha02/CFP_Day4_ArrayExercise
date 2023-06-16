72fff6065d8c840dcc2d72b70218ae49687d9c71
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-16 16:31:44 +0530

    Copy Array One to Another  Added

---
+++ CopyArrayToAnotherArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseArray
+{
+    internal class CopyArrayToAnotherArray
+    {
+      
+        public void Program()
+        {
+            Console.WriteLine("Enter the Length of array");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] array = new int[a];
+            int[] array2 = new int[a];
+
+            Console.WriteLine("Enter the Elements to array");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine($"The list of Array : ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+
+            Console.WriteLine($"The Copied list of Array :");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array2[i]}");
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,11 +16,15 @@ namespace ExerciseArray
 
             Console.WriteLine("Read n values in an array and display them in reverse order");
             StoreAndPrintInReverse p2 = new StoreAndPrintInReverse();
-            p2.Rev();*/
+            p2.Rev();
 
             Console.WriteLine("Sum of all array elements");
             SumOfElementsInArray p3 = new SumOfElementsInArray();
-            p3.Program3();
+            p3.Program3();*/
+
+            Console.WriteLine("Copy the elements of one array into another Array.");
+            CopyArrayToAnotherArray p4=new CopyArrayToAnotherArray();
+            p4.Program();
 
         }
    
