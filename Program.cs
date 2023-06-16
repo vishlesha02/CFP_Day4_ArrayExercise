b73f592b6edca3952325822e305ea9cc69bacf97
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-16 15:08:05 +0530

    Array Programs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseArray
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+             Console.WriteLine("Store elements in an array and print them");
+             StoreAndPrint p1 = new StoreAndPrint();
+             p1.StoreArray();
+
+        }
+   
+    }
+
+}
---
+++ StoreAndPrint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseArray
+{
+    class StoreAndPrint
+    {
+        public void StoreArray()
+        {
+            int[] a= new int[5];
+
+            Console.WriteLine("Enter 5 elements of the Array: ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The Elements of the array are : ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            
+            }
+        }
+    }
+}
